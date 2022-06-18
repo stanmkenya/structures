4c32216469bfd4f5907245b310f3742c63a477c7
stanmkenya <stanslausmwongela@gmail.com>
2022-06-18 13:25:09 +0300

    Update stacks.java

--- stacks.java
+++ stacks.java
@@ -1,1 +1,27 @@
+import java.util.EmptyStackException;
+//class name
+public class MyStack {
+    //main class
+    private static class StackNode{
+        // the data in each and every stack node
+        int data;
+        // the next node added 
+        StackNode next;
+//constructor
+    public StackNode (int data){
+        this.data = data;
 
+    }
+
+    }
+    //declare the top node
+    private StackNode top;
+//return item data at the top node
+    public int pop(){
+        //check
+        if (top ==null) throw new EmptyStackException();
+        top=top.next;
+        int item = top.data;
+        return item;
+
+    }
